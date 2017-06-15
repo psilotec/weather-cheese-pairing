9c04c2e611df19fd77a1a636289a19d67ac9d5f6
Scott DeVito <scottadevito@gmail.com>
2017-06-14 21:55:26 -0400

    Add react-native-elements styling to WeatherCheeseDisplay

--- src/components/weather_cheese_display.js
+++ src/components/weather_cheese_display.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native';
+import { View, StyleSheet, Image, Button, Alert } from 'react-native';
 import { Constants } from 'expo';
+import { Card, Text } from 'react-native-elements';
 
 export default class WeatherCheeseDisplay extends Component {
     _handleButtonPress = () => {
@@ -13,30 +14,33 @@ export default class WeatherCheeseDisplay extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.location}>
-                    Massapequa, NY
-                </Text>
-                <Text style={styles.date}>
+                <Text h2 style={styles.date}>
                     Monday, June 12th
                 </Text>
-
-                <View>
-                    <Text>Kinda Rainy</Text>
-                    <Image
-                        source={{ uri: 'http://www.northcountyplumbing.com/wp-content/uploads/2016/01/Tree-in-Rain.jpg' }}
-                        style={styles.weather}/>
-                </View>
-
-                <View>
-                    <Text>How about a nice Brie</Text>
-                    <Image
-                        source={{ uri: 'http://www.eatwisconsincheese.com/images/CheeseCupid/images/cheese/detail/brie.png' }}
-                        style={{ height: 140, width: 180 }} />
-                </View>
-
-                <Text style={styles.description}>
-                    How about a nice Brie? The delicous creaminess and subtle flavor will help you forget about the subtle shitiness outside.
+                <Text h4 style={styles.location}>
+                    Massapequa, NY
                 </Text>
+
+                <View style={styles.weather}>
+                    <Card
+                        title='Kinda Rainy'
+                        image={{uri: 'http://www.northcountyplumbing.com/wp-content/uploads/2016/01/Tree-in-Rain.jpg'}}>
+                        <Text style={{marginBottom: 10}}>
+                            The idea with React Native Elements is more about component structure than actual design.
+                        </Text>
+                    </Card>
+                </View>
+
+
+                <View>
+                    <Card
+                        title='Brie'
+                        image={{uri: 'http://www.eatwisconsincheese.com/images/CheeseCupid/images/cheese/detail/brie.png'}}>
+                        <Text style={{marginBottom: 5}}>
+                            How about a nice Brie? The delicous creaminess and subtle flavor will help you forget about the subtle shitiness outside.
+                        </Text>
+                    </Card>
+                </View>
 
                 <Button
                     title="Wine"
@@ -58,19 +62,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#ecf0f1',
     },
     location: {
-        margin: 0,
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
         color: '#34495e',
+        margin: 0,
     },
     date: {
-        marginBottom: 30,
     },
     weather: {
-        height: 140,
-        width: 200,
-        marginBottom: 20,
     },
     description: {
         marginLeft: 30,
